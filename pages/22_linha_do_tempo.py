18ed1897d2d07c6037ffc0d8d8169d1d779a2658
ILSGI <144830299+ILSGI@users.noreply.github.com>
2023-11-10 07:55:15 -0300

    Update 22_linha_do_tempo.py

--- pages/22_linha_do_tempo.py
+++ pages/22_linha_do_tempo.py
@@ -1,25 +1,43 @@
 import streamlit as st
 from streamlit_timeline import timeline
-import gettext
-_ = gettext.gettext
 import json
 from PIL import Image
 import io
 
 st.set_page_config(
     page_title="Antonio - Linha do Tempo",
-    page_icon="🏳️‍🌈"
+    page_icon=""
 )
 
 idioma = st.sidebar.selectbox("escolha o idioma", ['pt', 'en', 'es'])
-try:
-  localizator = gettext.translation('base', localedir='locales', languages=[idioma])
-  localizator.install()
-  _ = localizator.gettext 
-except:
-    pass
 
-st.title("Minha História")
-with open ('dados_antonio.json', "r", encoding="utf-8") as json_timeline:
-    timeline_info = json.load(json_timeline)     
+strings = {
+    "en": {
+        "titulo": "timeline",
+    },
+    "pt": {
+        "titulo": "linha do tempo",
+    },
+    "es": {
+        "titulo": "línea de tiempo",
+    }
+}
+
+def get_string(idioma, key):
+    return strings[idioma][key]
+
+titulo = get_string(idioma,"titulo")
+st.title(titulo)
+
+if idioma == "en" : 
+    with open ('dados_antonio_en.json', "r", encoding="utf-8") as json_timeline:
+        timeline_info = json.load(json_timeline)     
+elif idioma == "es" :
+    with open ('dados_antonio_es.json', "r", encoding="utf-8") as json_timeline:
+        timeline_info = json.load(json_timeline) 
+elif idioma == "pt" : 
+    with open ('dados_antonio_pt.json', "r", encoding="utf-8") as json_timeline:
+        timeline_info = json.load(json_timeline) 
+
+
 timeline(timeline_info, height=500, )
