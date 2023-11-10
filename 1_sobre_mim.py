7f7895804ba6a470cce2ef644b1f3a3731dd3d0d
ILSGI <144830299+ILSGI@users.noreply.github.com>
2023-11-10 08:32:32 -0300

    Update 1_sobre_mim.py

--- 1_sobre_mim.py
+++ 1_sobre_mim.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title="Antonio - sobre mim",
-    page_icon=""
+    page_icon="🏳️‍🌈"
 )
 
 idioma = st.sidebar.selectbox("escolha o idioma", ['pt', 'en', 'es'])
